de1dff45ca7651395de98dbc851d0370236ccf80
叶锞 <67529869+yk112233@users.noreply.github.com>
2024-07-29 10:59:46 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,6 +4,8 @@ bash ./tools/dist_train.sh \
    8 \
    --deterministic
 
+cp work_dirs/sparsedrive_small_stage1/latest.pth ckpt/sparsedrive_stage1.pth
+
 ## stage2
 bash ./tools/dist_train.sh \
    projects/configs/sparsedrive_small_stage2.py \
